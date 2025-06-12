be6fc74d73f66477420707a2146fc7ba341f6758
rakshitha <1nt22ec127.rakshitha@nmit.ac.in>
2025-06-12 20:53:27 +0530

    adde checkout slice and order slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,12 +4,15 @@ import authReducer from "./slices/authSlice"; // Adjust path as needed
 import productReducer from "./slices/productsSlice";
 import cartReducer from "./slices/cartSlice"
 import products from "../../../backend/data/products";
-
+import checkoutReducer from "./slices/checkoutSlice";
+import orderReducer from "./slices/orderSlice"
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products:productReducer,
     cart:cartReducer,
+    checkout:checkoutReducer,
+    orders:orderReducer,
   },
 });
 
